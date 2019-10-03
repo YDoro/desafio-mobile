7ec0ab5e6b26a6efd77b1644bed49b76a32343ac
Yan Mello <yan_mvd@hotmail.com>
2019-10-03 00:40:15 -0300

    flatlist com os produtos

--- desafioMobile/App/Views/Home/index.js
+++ desafioMobile/App/Views/Home/index.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
-import {Text, View, StatusBar,Dimensions} from 'react-native';
+import {Text, View, Alert, Dimensions, ActivityIndicator,Image} from 'react-native';
 import {withNavigation} from 'react-navigation';
-import {Button, Icon, Container} from 'native-base';
+import {Button, Icon, Container, Row} from 'native-base';
 import CustomHeader from '../../Components/Header';
-import { ScrollView } from 'react-native-gesture-handler';
+import {ScrollView, FlatList} from 'react-native-gesture-handler';
 import SideMenu from 'react-native-side-menu';
 import SideBar from '../../Components/SideBar';
-
+import api from '../../Services/API/index';
+const numColumns = 2;
+function Item({produto}) {
+  return (
+    <View style={{borderWidth:1, width: '48%', borderRadius: 15, margin:"1%"}}>
+       <View>
+        <Image
+          source={{uri: produto.Skus[0].Images[0].ImageUrl}}
+          style={{
+            height: 300,
+            width: 300,
+            transform: [{scale: 0.45}],
+            alignSelf: 'center',
+          }}
+        />
+      </View>
+      <Text style={{paddingHorizontal: 10}}>{produto.Name}</Text>
+    </View>
+  );
+}
 class Home extends Component {
   static navigationOptions = {
     header: null,
@@ -14,34 +33,64 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen:false
+      isLoading: true,
+      isOpen: false,
+      produtos: [],
     };
   }
-  componentDidMount(){
+  componentDidMount() {
     const isOpen = this.refs.header.props.isOpen;
-    this.setState({isOpen});
+    this.getData();
   }
-  render() {
-    const menu = <SideBar/>
-    return (
-      <SideMenu menu={menu}
-      edgeHitWidth={Dimensions.get('window').width}
-      toleranceY={30}
-      isOpen={this.state.isOpen}
-      
-      >
-      <View>
-        <CustomHeader ref='header' isOpen={()=>this.setState({isOpen:!this.state.isOpen})}/>
+  getData = async () => {
+    await api
+      .post('Search/Criteria', {
+        Query: '',
+        Offset: 0,
+        Size: 10,
+      })
+      .then(async response => {
+        console.log(response);
+        this.setState({produtos: response.data.Products, isLoading: false});
+      })
+      .catch(err => {
+        Alert.alert('erro', JSON.stringify(err.response.data));
+      });
+  };
+  renderItem = produto => {
+    return <Text>{produto.Name}</Text>;
+  };
 
-        <ScrollView>
-          
-          
-           <View >
-          <Text>Teste</Text>
+  render() {
+    const menu = <SideBar />;
+    return (
+      <SideMenu
+        menu={menu}
+        edgeHitWidth={Dimensions.get('window').width}
+        toleranceY={30}
+        isOpen={this.state.isOpen}>
+        <View>
+          <CustomHeader
+            ref="header"
+            isOpen={() => this.setState({isOpen: !this.state.isOpen})}
+          />
+
+          <ScrollView>
+            <View style={{width: '100%',alignSelf:"center"}}>
+              {this.state.isLoading ? (
+                <ActivityIndicator size="large" color="#0000ff" />
+              ) : (
+                <FlatList
+                  data={this.state.produtos}
+                  
+                  keyExtractor={item => item.Id}
+                  numColumns={numColumns}
+                  renderItem={({item}) => <Item produto={item} />}
+                />
+              )}
+            </View>
+          </ScrollView>
         </View>
-        </ScrollView>
-       
-      </View>
       </SideMenu>
     );
   }
