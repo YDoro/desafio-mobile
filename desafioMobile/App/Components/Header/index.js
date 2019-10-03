bf670165fec96543ea199bc3006dd637b24daa61
Yan Mello <yan_mvd@hotmail.com>
2019-10-02 21:53:07 -0300

    barra de busca

--- desafioMobile/App/Components/Header/index.js
+++ desafioMobile/App/Components/Header/index.js
@@ -1,49 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {StatusBar} from 'react-native';
-import {   Title,  View } from 'native-base';
-import styles from './styles'
+import {Title, View, Input, Item} from 'native-base';
+import styles from './styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class CustomHeader extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searching:false
-            
-        };
-      }
-      
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searching: false,
+    };
+  }
 
+  render() {
     return (
-     
-        <View style={styles.Header}>
-          <StatusBar
-            backgroundColor="#333"
-            barStyle="light-content"
-            animated={true}
-          />
-        <View style={styles.iconDiv}>
-        <Icon name="bars"
-          active
-          style={styles.menu}
-          onPress={() =>{this.props.isOpen()}} >
-        </Icon>
-        </View>
-        <Title style={styles.title} >Desafio Mobile</Title>
-        <View style={styles.back2}>
+      <View>
+      <View style={styles.Header}>
+        <StatusBar
+          backgroundColor="#333"
+          barStyle="light-content"
+          animated={true}
+        />
         <View style={styles.iconDiv}>
           <Icon
-            name="shopping-bag"
+            name="bars"
             active
-            style={styles.icon}
-          >
-          </Icon>
+            style={styles.menu}
+            onPress={() => {
+              this.props.isOpen();
+            }}></Icon>
+        </View>
+        <Title style={styles.title}>Desafio Mobile</Title>
+        <View style={styles.back2}>
+          <View style={styles.iconDiv}>
+            <Icon
+              name="search"
+              active
+              style={styles.icon}
+              onPress={() => {
+                this.setState({searching: !this.state.searching});
+              }}></Icon>
           </View>
         </View>
-
       </View>
-     
+
+{this.state.searching && 
+  <View style={styles.searchBar}>
+    <Item rounded bordered style={styles.searchInputIten}>
+      <Icon active name='search' style={styles.searchInputIcon} />
+      <Input style={styles.searchInput} />
+    </Item>
+  </View>
+  
+  }
+  </View>
     );
+
   }
 }
--- desafioMobile/App/Components/Header/styles.js
+++ desafioMobile/App/Components/Header/styles.js
@@ -19,5 +19,35 @@ const styles = StyleSheet.create({
     position: 'relative',
     backgroundColor: '#333',
   },
+  searchBar:{
+    marginTop:5,
+    paddingBottom:5,
+    borderBottomWidth:1,
+    borderColor:"#EEE"    
+
+  },
+  searchInputIten:{
+    borderColor:"#FFF",
+    height:Dimensions.get("window").height*0.0455,
+    marginLeft:Dimensions.get("window").width*0.04,
+    marginRight:Dimensions.get("window").width*0.04,
+    alignSelf:"center",
+    backgroundColor:'#DDD',
+    color:"#FFF"
+  },
+  searchInputIcon:{
+    color:"#FFF",
+    paddingLeft:10,
+    borderRightWidth:1,
+    borderColor:"#FFF",
+    transform:[
+      {scale:1.5}
+    ],
+    paddingRight:5
+  },
+  searchInput:{
+    color:"#FFF",
+    paddingLeft:10
+  },
 });
 export default styles;
